306c08448ec1eacd6b56271e479f7ab8f777c6a1
Andy <lhr325803176@163.com>
2022-04-29 17:58:03 +0800

    Writing Webget

--- apps/webget.cc
+++ apps/webget.cc
@@ -16,9 +16,19 @@ void get_URL(const string &host, const string &path) {
     // Then you'll need to print out everything the server sends back,
     // (not just one call to read() -- everything) until you reach
     // the "eof" (end of file).
-
-    cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
-    cerr << "Warning: get_URL() has not been implemented yet.\n";
+    TCPSocket sock1;
+    sock1.connect(Address(host, "http"));
+    string messages;
+    messages += "GET "+path+" HTTP/1.1\r\n";
+    messages += "Host: " + host + "\r\n";
+    messages += "Connection: close\r\n\r\n";
+    sock1.write(messages);
+    while(!sock1.eof()) {
+        cout << sock1.read();
+    }
+    sock1.close();
+    // cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
+    // cerr << "Warning: get_URL() has not been implemented yet.\n";
 }
 
 int main(int argc, char *argv[]) {
